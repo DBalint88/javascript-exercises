4b45b3b4e5bcef9f1482cd54bf54cd7bf3533cc7
DBalint88 <dbalint88@gmail.com>
2022-08-16 17:48:07 -0400

    Solve the first five problems.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = "";
+    for (let j = str.length -1; j>=0; j--) {
+        result += str[j];
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start === "number" && start > 0 && typeof end === "number" && end > 0) {
+        result = 0;
 
+        if (start > end) {
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+    
+        for (let i = start; i <= end; i++) {
+            result += i;      
+        }
+    
+        return result;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
