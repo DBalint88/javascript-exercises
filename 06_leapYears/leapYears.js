d66ec46d34f973b8a7a50d3e09ed01ef0c7ea91d
DBalint88 <dbalint88@gmail.com>
2022-08-17 08:55:48 -0400

    Complete exercises 06 & 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if it's divisible by 4 is the first step.
+    //if it happens to also be divisible by 100, then...
+    // if it's divisible by 400
 
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        } 
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  let tempc = Math.round((tempf - 32) * 5 / 9 * 10) / 10;
+  return tempc;
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  let tempf = Math.round(((tempc * 9 / 5) + 32) * 10) / 10;
+  return tempf;
 };
 
 // Do not edit below this line
